d5f1df4488d144f0121048c14b4d1b7b21fce180
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-07-12 21:15:19 -0400

    Got the scrambler working. Made it mildly pretty.

--- Scrambler/ContentView.swift
+++ Scrambler/ContentView.swift
@@ -9,13 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var scramble: String
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+                self.scramble = self.generateScramble()
+            }) {
+                Text(scramble)
+                    .bold()
+                    .font(.system(size: 40))
+            }
+            .padding()
+            Spacer()
+        }
+    }
+    
+    
+    func generateScramble() -> String {
+        let moves = ["R", "L", "F", "B", "U", "D"]
+        let opposites = ["L", "R", "B", "F", "D", "U"]
+        let modifiers = ["'", "2", ""]
+        var scramble: [String] = []
+        var ultimate: String = ""
+        var penultimate: String = ""
+        var current: String = ""
+        
+        while scramble.count < 20 {
+            //used to keep the loop going if I break it. apparently a simple "redo" is too much to ask for.
+            while scramble.count < 20 {
+                current = moves.randomElement()!
+                
+                if current == ultimate {
+                    break
+                } else if current == penultimate && ultimate == opposites[moves.firstIndex(of: current) ?? 0] {
+                    break
+                } else {
+                    penultimate = ultimate
+                    ultimate = current
+                    scramble.append(current + modifiers.randomElement()!)
+                }
+            }
+            
+        }
+        let result = scramble.joined(separator: " ")
+        return result
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(scramble: "R U R' U'")
     }
 }
